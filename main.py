b75dd69b62eb0f510154749aa354b5a18bfcfd6e
Wakey222 <winrarich@gmail.com>
2024-06-17 19:58:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# Comment
-print("Привет, меня зовут Вейки")
+name = Вейки
+print("Имя", name)
+
+surname = Вейкович
+print("Фамилия", surname)
+
+wish = Хорошего дня
+print("Пожелание", wish)
+
+def great():
+    print("Hello Wakey, have good day!")
